f468ee84d1640289b732c332e1c107fc7e282c60
Afidhu <afidhumatata81@gmail.com>
2025-01-12 02:41:36 +0300

    added allowed host

--- backend/social/settings.py
+++ backend/social/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blogs-platform.onrender.com', '127.00.1', 'localhost']
 
 
 # Application definition
