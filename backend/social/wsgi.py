22723463f48228172997b609b0a2bba9080800a5
Afidhu <afidhumatata81@gmail.com>
2025-01-13 22:33:05 +0300

    added allowed host2

--- backend/social/wsgi.py
+++ backend/social/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('C:\Users\Afidh\OneDrive\Desktop\STACK_SOCIAL_MEDIA\backend\social')
 
 from django.core.wsgi import get_wsgi_application
 
